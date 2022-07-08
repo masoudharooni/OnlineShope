5be5ef6ced6b3e55db6eeeca5f2d67758860edc4
masoud harooni <masoudharooni50@gmail.com>
2022-07-08 20:05:01 +0430

    [CREATE] users table completed

--- database/migrations/2022_07_08_152423_create_products_table.php
+++ database/migrations/2022_07_08_152423_create_products_table.php
@@ -13,15 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->char('mobile', 11);
-            $table->string('password');
-            $table->enum('role', ['admin', 'user']);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
