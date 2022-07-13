d4243f3ecf6902b690ef1ad1c60c41c7db0b467d
masoud harooni <masoudharooni50@gmail.com>
2022-07-13 13:40:48 +0430

    [REF] refactor Image::upload method

--- app/Utilities/Uploaders/Image.php
+++ app/Utilities/Uploaders/Image.php
@@ -7,18 +7,31 @@ use Illuminate\Support\Facades\Storage;
 
 class Image
 {
-    public static function upload($image, $type, $basePath)
+    public static function upload($image, $type, $basePath): string|false
     {
-        if (!in_array($type, ['thumbnail', 'source', 'demo']))
+        if (!self::imageTypeValidator($type))
             dd('type file is not allowed!');
-        $diskName = 'public_storage';
-        if ($type == 'source')
-            $diskName = 'local_storage';
+        $diskName = self::diskNameCreator($type);
 
         $path = $basePath . $type . "_" . $image->getClientOriginalName();
         $uploadResult = Storage::disk($diskName)->put($path, File::get($image));
         if (!$uploadResult)
             return false;
         return $path;
+    }
+
+    private static function imageTypeValidator(string $type): bool
+    {
+        if (!in_array($type, ['thumbnail', 'source', 'demo']))
+            return false;
+        return true;
+    }
+
+    private static function diskNameCreator(string $type): string
+    {
+        $diskName = 'public_storage';
+        if ($type == 'source')
+            $diskName = 'local_storage';
+        return $diskName;
     }
 }
